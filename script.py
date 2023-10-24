9180f8f2d71ae9ec01312b4da299bf22698f2ff7
2b04.2123491.shaunho <2b04.2123491.shaunho@gmail.com>
2023-10-24 16:46:24 +0800

    more experimentations on NER

--- script.py
+++ script.py
@@ -31,8 +31,8 @@ else:
     #extract text for pdf
     if file_type == "PDF":    
         text = extract_text(file_path)
-        #print("*************Successfully extracted PDF*************")
-        #print(text)
+        # print("*************Successfully extracted PDF*************")
+        # print(text)
     elif file_type == "DOCX": #10 Million Bytes for 10MB
         text = docx2txt.process(file_path)
         #print("*************Successfully extracted Word File*************")
@@ -40,26 +40,6 @@ else:
     else:
         #File type is not accepted
         raise Exception("*************Filetype is not accepted*************")
-
-
-
-# nltk library
-# import nltk
-# from nltk.tokenize import word_tokenize
-# from nltk.tag import pos_tag
-
-# def preprocess(sent):
-#     sent = nltk.word_tokenize(sent)
-#     sent = nltk.pos_tag(sent)
-#     return sent
-
-# sent = preprocess(text)
-# print(sent)
-
-# pattern = 'NP: {<DT>?<JJ>*<NN>}'
-# cp = nltk.RegexpParser(pattern)
-# cs = cp.parse(sent)
-# print(cs)
 
 
 
@@ -104,32 +84,78 @@ def extract_name_from_resume(text):
 
     return name
 
-# if __name__ == '__main__':
-#     resume_text = text
+def extract_education_from_resume(text):
+    education = []
 
-#     name = extract_name_from_resume(resume_text)
-#     if name:
-#         print("Name:", name)
-#     else:
-#         print("Name not found")
+    # Use regex pattern to find education information
+    pattern = r" /([A-Z][^\s,.]+[.]?\s[(]?)*(College|University|Institute|Law School|School of|Academy|M.Sc|B.Sc)[^,\d]*(?=,|\d)/"
+    match = re.search(pattern, text)
+    if match:
+        education = match.group()
 
-#     contact_number = extract_contact_number_from_resume(resume_text)
-#     if contact_number:
-#         print("Contact Number:", contact_number)
-#     else:
-#         print("Contact Number not found")
+    return education
 
-#     email = extract_email_from_resume(resume_text)
-#     if email:
-#         print("Email:", email)
-#     else:
-#         print("Email not found")
+import re
+
+def extract_skills_from_resume(text, skills_list):
+    skills = []
+
+    for skill in skills_list:
+        pattern = r"\b{}\b".format(re.escape(skill))
+        match = re.search(pattern, text, re.IGNORECASE)
+        if match:
+            skills.append(skill)
+
+    return skills
+
+import pandas as pd
+
+if __name__ == '__main__':
+    resume_text = text
+
+    name = extract_name_from_resume(resume_text)
+    if name:
+        print("Name:", name)
+    else:
+        print("Name not found")
+
+    contact_number = extract_contact_number_from_resume(resume_text)
+    if contact_number:
+        print("Contact Number:", contact_number)
+    else:
+        print("Contact Number not found")
+
+    email = extract_email_from_resume(resume_text)
+    if email:
+        print("Email:", email)
+    else:
+        print("Email not found")
+
+    education = extract_education_from_resume(resume_text)
+    if education:
+        print("Education:", education)
+    else:
+        print("Education not found")
+
+    # List of predefined skills
+    skills_list = pd.read_csv('skills.csv')
+    #print(skills_list)
+    #skills_list = ['Python', 'Data Analysis', 'Machine Learning', 'Communication', 'Project Management', 'Deep Learning', 'SQL', 'Tableau']
+
+    extracted_skills = extract_skills_from_resume(text, skills_list)
+
+    if extracted_skills:
+        print("Skills:", extracted_skills)
+    else:
+        print("No skills found")
 
 
 
-import spacy                                                                                                                            
+# import spacy                                                                                                                            
 
-nlp = spacy.load('en_core_web_trf')                                                                                                                  
-sents = nlp(text) 
-people = [ee for ee in sents.ents if ee.label_ == 'PERSON']        
-print(people)
+# nlp = spacy.load('en_core_web_trf')                                                                                                                  
+# sents = nlp(text) 
+# people = [ee for ee in sents.ents if ee.label_ == 'PERSON']        
+# print(people)
+
+
